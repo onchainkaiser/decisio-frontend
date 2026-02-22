1851ded992b14527e6c18ecfa6d21863d039b1df
onchainkaiser <webkaiser8@gmail.com>
2026-02-22 13:24:17 +0100

    update api url to production backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://decisio-backend.pxxl.click',
 })
 
-// Automatically attach token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
